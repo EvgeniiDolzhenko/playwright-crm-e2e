039ab032801ea22c1d225d390e090d08ccdc0c49
Eugene <zhrobot@gmail.com>
2024-04-27 12:01:37 -0400

    fix

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -18,6 +18,7 @@ test.describe('Negative scenarios',async ()=>{
     await page.locator('#normal_login_email').fill('email')
     await page.locator('#normal_login_password').fill('password')
     await expect( page.locator('.ant-form-item-explain-error')).toBeVisible();
+    await page.close()
   })
 
   test('Required error', async ({page}) => {
@@ -25,9 +26,11 @@ test.describe('Negative scenarios',async ()=>{
     await page.locator('#normal_login_email').clear()
     await expect( page.locator('.ant-form-item-explain-error')).toBeVisible()
     await expect( page.locator('.ant-form-item-explain-error')).toHaveCSS('color','rgb(255, 77, 79)')
+    await page.close()
   })
 
 })
+
 
 test.describe('Verify api login',()=>{
 
@@ -38,31 +41,32 @@ test.describe('Verify api login',()=>{
 
   test('Verify API login',async({page})=>{
     await page.goto(url+'client')
+    const response = await page.waitForResponse('**/client/search')
     expect(page.url()).toContain('/client')
+    console.log(await response.json())
+    await page.close()
   })
 
   test('Verify order page',async({page})=>{
     await page.goto(url+'order')
+    await page.waitForResponse('**/order/search')
     expect(page.url()).toContain('/order')
+    await page.close()
   })
 
   test('Verify vendor page',async({page})=>{
-    const loginPage = new LoginPage(page)
-    await loginPage.apiLogin(apiUrl, email, pass,url)
     await page.goto(url+'vendor')
+    await page.waitForResponse('**/vendor/search')
     expect(page.url()).toContain('/vendor')
+    await page.close()
   })
 
   test('Verify service page',async({page})=>{
-    const loginPage = new LoginPage(page)
-    await loginPage.apiLogin(apiUrl, email, pass,url)
     await page.goto(url+'service')
+    await page.waitForResponse('**/service/search')
     expect(page.url()).toContain('/service')
+    await page.close()
   })
-
-
-
-  
  
 })
 
